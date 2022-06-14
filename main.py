9fdb63ef2fe24d360736f828392bce1636c3ab9a
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-06-14 23:58:02 +0300

    Add universal wine list support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
@@ -6,17 +7,32 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 WINERY_FOUNDING_YEAR = 1920
 
-if __name__ == '__main__':
-    winery_age = datetime.date.today().year - WINERY_FOUNDING_YEAR
-    if 0 < winery_age % 10 < 5:
-        winery_age = f'{winery_age} года'
+
+def get_winery_age(founding_year):
+    age = datetime.date.today().year - founding_year
+    if 0 < age % 10 < 5:
+        return f'{age} года'
     else:
-        winery_age = f'{winery_age} лет'
+        return f'{age} лет'
+
+
+def sort_wines(wines_data_frame):
+    wines_serial = wines_data_frame.to_dict(orient='records')
+    wines = defaultdict(list)
+    for wine in wines_serial:
+        key = list(wine.values())[0]
+        wines[key].append(wine)
+    return wines
+
+
+if __name__ == '__main__':
+    winery_age = get_winery_age(WINERY_FOUNDING_YEAR)
     wines_df = pandas.read_excel(
-        'wine.xlsx',
-        names=['title', 'grape', 'price', 'image'],
+        io='wine2.xlsx',
+        na_values='nan',
+        keep_default_na=False,
     )
-    wines = wines_df.to_dict(orient='records')
+    sorted_wines = sort_wines(wines_df)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -24,10 +40,9 @@ if __name__ == '__main__':
     template = env.get_template('template.html')
     rendered_page = template.render(
         age=winery_age,
-        wines=wines,
+        wines=sorted_wines,
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
