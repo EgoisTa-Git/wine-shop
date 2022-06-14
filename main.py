a6bf9e860a5b11c3bad4650074c67e5b074a3200
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-06-14 21:40:13 +0300

    Add .xlsx support for wine list

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 WINERY_FOUNDING_YEAR = 1920
-winery_age = datetime.date.today().year - WINERY_FOUNDING_YEAR
-if 0 < winery_age % 10 < 5:
-    winery_age = f'{winery_age} года'
-else:
-    winery_age = f'{winery_age} лет'
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    winery_age = datetime.date.today().year - WINERY_FOUNDING_YEAR
+    if 0 < winery_age % 10 < 5:
+        winery_age = f'{winery_age} года'
+    else:
+        winery_age = f'{winery_age} лет'
+    wines_df = pandas.read_excel(
+        'wine.xlsx',
+        names=['title', 'grape', 'price', 'image'],
+    )
+    wines = wines_df.to_dict(orient='records')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        age=winery_age,
+        wines=wines,
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    age=winery_age,
-    image='',
-    title='',
-    grape='',
-    price='',
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
